4b49d7567cde5e5ad7930d2ec19524e199a5c2da
Roman Tupikov <romkacg@gmail.com>
2017-10-31 04:42:50 +0300

    libGDX init

---
+++ android/src/com/romantupikov/game/simplerpg/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.romantupikov.game.simplerpg;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.romantupikov.game.simplerpg.SimpleRpgGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SimpleRpgGame(), config);
+	}
+}
---
+++ desktop/src/com/romantupikov/game/simplerpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.romantupikov.game.simplerpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.romantupikov.game.simplerpg.SimpleRpgGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SimpleRpgGame(), config);
+	}
+}
